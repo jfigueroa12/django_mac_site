96212f1e62e4d1c2c211be6798e7c3fe2891fd32
flyasth3sky <jonathan.figueroa@zoho.com>
2019-09-02 22:17:42 -0400

    Updated Search Page css stylings

--- django_mac_site/pages/forms.py
+++ django_mac_site/pages/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class InfoForm(forms.Form):
     serial = forms.CharField(label='', max_length=20, widget=forms.TextInput(
-    attrs={'class': 'form-control', 'placeholder': 'Enter WSID or serial #'}))
+    attrs={'id': 'search-field', 'class': 'form-control searchTerm', 'placeholder': 'Enter WSID or Serial #'}))
